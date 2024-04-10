02ff724a9f32d2c8d6c1d39ca13f2c2cea3b5830
4-en <ben.schumacher0@gmail.com>
2024-04-10 15:30:40 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+deps = [
+    "openai"
+]
+
+setup(
+    name="cli-gpt",
+    version="0.1",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=deps,
+    entry_points={
+        'console_scripts': [
+            'cli-gpt = main:main'
+        ]
+    }
+)
+
+
